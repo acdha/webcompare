b95877c7aae03d538d71059fc105a8b38e719865
Chris Adams <chris@improbable.org>
2011-09-01 17:08:24 -0400

    PEP8 cleanup
    * Line lengths are now <100
    * Logging calls avoid forcing early string interpolation

--- webcompare.py
+++ webcompare.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python
+# encoding: utf-8
+from __future__ import absolute_import
 
 from difflib import SequenceMatcher
 from optparse import OptionParser
 from urlparse import urlparse
-
 import httplib
 import json
 import logging
-import lxml.html
-from lxml.etree import XPath
 import os
 import re                       # "now you've got *two* problems"
 import sys
 import time
 import urllib2
+
+from lxml.etree import XPath
 import html5lib
+import lxml.html
 
 
 class Result(object):
@@ -37,55 +39,59 @@ class Result(object):
                  comparisons={}):
 
         self.result_type = self.__class__.__name__
-        self.origin_url  = origin_url
+        self.origin_url = origin_url
         self.origin_code = int(origin_code)
         self.origin_time = origin_time
         self.origin_html_errors = origin_html_errors
-        self.target_url  = target_url
+        self.target_url = target_url
         self.target_code = target_code
         self.target_time = target_time
         self.target_html_errors = target_html_errors
         self.comparisons = comparisons
         if not isinstance(self.result_type, basestring):
-            raise TypeError, "result_type must be a string"
+            raise TypeError("result_type must be a string")
         if not isinstance(self.origin_url, basestring):
-            raise TypeError, "origin_url must be a string"
+            raise TypeError("origin_url must be a string")
 
         if self.origin_code != None and type(self.origin_code) != int:
-            raise TypeError, "origin_code=%s must be a int" % self.origin_code
+            raise TypeError("origin_code=%s must be a int" % self.origin_code)
         if self.origin_time != None and type(self.origin_time) != float:
-            raise TypeError, "origin_time=%s must be a float" % self.origin_time
+            raise TypeError("origin_time=%s must be a float" % self.origin_time)
         if self.origin_html_errors != None and type(self.origin_html_errors) != list:
-            raise TypeError, "origin_html_errors=%s must be a list (of errors)" % self.origin_html_errors
+            raise TypeError("origin_html_errors=%s must be a list (of errors)" % self.origin_html_errors)
         if self.target_url != None and not hasattr(self.target_url, "lower"):
-            raise TypeError, "target_url=%s must be a string" % self.target_url
+            raise TypeError("target_url=%s must be a string" % self.target_url)
         if self.target_code != None and type(self.target_code) != int:
-            raise TypeError, "target_code=%s must be a int" % self.target_code
+            raise TypeError("target_code=%s must be a int" % self.target_code)
         if (self.target_time != None and type(self.target_time) != float):
-            raise TypeError, "target_time=%s must be a float" % self.target_time
+            raise TypeError("target_time=%s must be a float" % self.target_time)
 
         if self.target_html_errors != None and type(self.target_html_errors) != list:
-            raise TypeError, "target_html_errors=%s must be an list (of errors)" % self.target_html_errors
+            raise TypeError("target_html_errors=%s must be an list (of errors)" % self.target_html_errors)
 
         if not isinstance(self.comparisons, dict):
-            raise TypeError, "comparisons=%s must be a dict" % self.comparisons
-
+            raise TypeError("comparisons=%s must be a dict" % self.comparisons)
 
     def __str__(self):
-        return "<%s o=%s oc=%s t=%s tc=%s comp=%s>" % (
-            self.result_type,
-            self.origin_url, self.origin_code,
-            self.target_url, self.target_code,
-            self.comparisons)
+        return "<%s o=%s oc=%s t=%s tc=%s comp=%s>" % (self.result_type,
+                                                       self.origin_url,
+                                                       self.origin_code,
+                                                       self.target_url,
+                                                       self.target_code,
+                                                       self.comparisons)
+
 
 class ErrorResult(Result):
     pass
 
+
 class BadOriginResult(Result):
     pass
 
+
 class BadTargetResult(Result):
     pass
+
 
 class GoodResult(Result):
     pass
@@ -113,7 +119,7 @@ class Response(object):
 
         try:
             self.content_length = int(self.http_response.headers['content-length'])
-        except KeyError, e:
+        except KeyError:
             self.content_length = len(self.content)
 
         if self.content_type.startswith("text/html"):
@@ -181,8 +187,8 @@ class Walker(object):
         TODO: do I want to handle relative origin_urls?
         """
         if not origin_url.startswith(self.origin_url_base):
-            raise ValueError, "origin_url=%s does not start with origin_url_base=%s" % (
-                origin_url, self.origin_url_base)
+            raise ValueError("origin_url=%s does not start with origin_url_base=%s" % (
+                             origin_url, self.origin_url_base))
         return origin_url.replace(self.origin_url_base, self.target_url_base, 1)
 
     def _is_within_origin(self, url):
@@ -201,20 +207,6 @@ class Walker(object):
             url = re.sub(ignorere, "", url)
         return url
 
-    def _get_urls(self, html, base_href): # UNUSED?
-        """Return list of objects representing absolute URLs found in the html.
-        [(element, attr, link, pos) ...]
-        TODO: May want to normalize these
-        - Need to make URLs absolute, take into account this doc's URL as base (?)
-        - .resolve_base_href()
-        - .make_links_absolute(base_href, resolve_base_href=True)
-        - See absolutizing here:
-          http://blog.ianbicking.org/2008/12/10/lxml-an-underappreciated-web-scraping-library
-        """
-        tree = lxml.html.fromstring(html)
-        tree.make_links_absolute(base_href, resolve_base_href=True)
-        return tree.iterlinks()
-
     def add_comparator(self, comparator_function):
         """Add a comparator method to the list of comparators to try.
         Each comparator should return a floating point number between
@@ -228,16 +220,18 @@ class Walker(object):
         Hopefully will allow clever JS tricks to render and sort in browser.
         """
         stats = {}
-        #import pdb; pdb.set_trace()
+
         for r in self.results:
             stats[r.result_type] = stats.get(r.result_type, 0) + 1
         result_list = [r.__dict__ for r in self.results]
         all_results = dict(results=dict(resultlist=result_list, stats=stats))
+
         try:
             json_results = json.dumps(all_results, sort_keys=True, indent=4)
-        except UnicodeDecodeError, e:
-            logging.error("Unicode error %s" % e)
-            import pdb; pdb.set_trace() # wait for a human!!!!!!!!!
+        except UnicodeDecodeError as e:
+            logging.error("Unicode error: %s", e, exc_info=True)
+            raise
+
         return json_results
 
     def walk_and_compare(self):
@@ -257,13 +251,15 @@ class Walker(object):
                 t = time.time()
                 origin_response = self._fetch_url(origin_url)
                 origin_time = time.time() - t
-            except (urllib2.URLError, httplib.BadStatusLine), e:
+            except (urllib2.URLError, httplib.BadStatusLine) as e:
                 logging.warning("Could not fetch origin_url=%s -- %s" % (origin_url, e))
-                result = ErrorResult(origin_url, getattr(e, 'code', 0)) # no HTTP code if DNS not found
+                # We won't have an HTTP code for low-level network failures:
+                result = ErrorResult(origin_url, getattr(e, 'code', 0))
                 self.results.append(result)
                 logging.info("result(err resp): %s" % result)
                 continue
-            if origin_response.code != 200: # TODO: do I need this check? or code block?
+            # TODO: do I need this check? or code block?
+            if origin_response.code != 200:
                 result = BadOriginResult(origin_url, origin_response.code)
                 self.results.append(result)
                 logging.warning(result)
@@ -274,10 +270,12 @@ class Walker(object):
 
                     for url_obj in origin_response.htmltree.iterlinks():
                         url = self._normalize_url(url_obj[2])
+
                         if not self._is_within_origin(url):
-                            logging.debug("Skip url=%s not within origin_url=%s" % (url, self.origin_url_base))
+                            logging.debug("Skip url=%s not within origin_url=%s",
+                                          url, self.origin_url_base)
                         elif url not in self.origin_urls_todo and url not in self.origin_urls_visited:
-                            logging.debug("adding URL=%s" % url)
+                            logging.debug("adding URL=%s", url)
                             self.origin_urls_todo.append(url)
                 target_url = self._get_target_url(origin_url)
                 logging.debug("about to fetch target_url=%s" % target_url)
@@ -309,10 +307,10 @@ class Walker(object):
                         e.getparent().remove(e)
 
                 if origin_response.htmltree == None or target_response.htmltree == None:
-                    logging.warning("compare: None for origin htmltree=%s or target htmltree=%s" % (
-                            origin_response.htmltree, target_response.htmltree))
-                    target_html_errors=[]
-                    comparisons={}
+                    logging.warning("compare: None for origin htmltree=%s or target htmltree=%s",
+                                    origin_response.htmltree, target_response.htmltree)
+                    target_html_errors = []
+                    comparisons = {}
                 else:
                     target_html_errors = target_response.get_parser_errors()
 
@@ -330,17 +328,6 @@ class Walker(object):
                 self.results.append(result)
                 logging.info(result)
 
-
-
-# TODO: instantiation and invocation of Normalizer and Comparator feels stilted and awkward.
-
-class Normalizer(object):
-    """TODO: should I be subclassing an LXML stipper? (oh baby)
-    """
-    def __init__(self, htmlstring):
-        self.htree = lxml.html.fromstring(htmlstring)
-    def normalize(self):
-        return self.htree.text_content().lower() # TODO removes spaces implied by tags??
 
 class Comparator(object):
     """Compare HTML trees, return number 0-100 representing less-more similarity.
@@ -379,7 +366,8 @@ class Comparator(object):
     def compare(self, origin_response, target_response):
         """This is expected to be subclassed and then superclass invoked.
         """
-        raise RuntimeError, "You need to subclass class=%s" % self.__class__.__name__
+        raise RuntimeError("You need to subclass class=%s" % self.__class__.__name__)
+
 
 class TitleComparator(Comparator):
     """Compare <title> content from the reponse in a fuzzy way.
@@ -390,14 +378,16 @@ class TitleComparator(Comparator):
         try:
             origin_title = origin_response.htmltree.xpath("//html/head/title")[0].text
             target_title = target_response.htmltree.xpath("//html/head/title")[0].text
-        except IndexError, e:
-            logging.warning("Couldn't find a origin_title=%s or target_title=%s" % (origin_title, target_title))
+        except IndexError:
+            logging.warning("Couldn't find a origin_title=%s or target_title=%s", origin_title, target_title)
             return self.match_nothing
         return self.fuzziness(origin_title, target_title)
+
 
 class ContentComparator(Comparator):
     def compare(self, origin_response, target_response):
         return self.fuzziness(origin_response.content, target_response.content)
+
 
 class BodyComparator(Comparator):
     def compare(self, origin_response, target_response):
@@ -405,10 +395,12 @@ class BodyComparator(Comparator):
         try:
             origin_body = origin_response.htmltree.xpath("//html/body")[0].text_content().lower()
             target_body = target_response.htmltree.xpath("//html/body")[0].text_content().lower()
-        except (IndexError, AttributeError), e:
-            logging.warning("Couldn't find a origin_body=%s or target_body=%s" % (origin_body, target_body))
+        except (IndexError, AttributeError):
+            logging.warning("Couldn't find a origin_body=%s or target_body=%s",
+                            origin_body, target_body)
             return self.match_nothing
         return self.fuzziness(origin_body, target_body)
+
 
 class LengthComparator(Comparator):
     def compare(self, origin_response, target_response):
@@ -429,19 +421,25 @@ class LengthComparator(Comparator):
 if __name__ == "__main__":
     usage = 'usage: %prog [options] origin_url target_url   (do: "%prog --help" for help)'
     parser = OptionParser(usage)
-    parser.add_option("-v", "--verbose", action="count", default=0, dest="verbose", help="log info about processing")
-    parser.add_option("--debug", action="store_true", default=False, help="Launch interactive debugger on failures")
-    parser.add_option("-f", "--file", dest="filename", help="path to store the json results to (default is stdout)")
+    parser.add_option("-v", "--verbose", action="count", default=0, dest="verbose",
+                      help="log info about processing")
+    parser.add_option("--debug", action="store_true", default=False,
+                      help="Launch interactive debugger on failures")
+    parser.add_option("-f", "--file", dest="filename",
+                      help="path to store the json results to (default is stdout)")
     parser.add_option("-i", "--ignorere", dest="ignoreres", action="append", default=[],
                       help="Ignore URLs matching this regular expression, can use multiple times")
     parser.add_option("-I", "--ignorere-file", dest="ignorere_file",
                       help="File containtaining regexps specifying URLs to ignore, one per line")
     parser.add_option("--origin-noise-xpath-file",
-                      help="File containing XPath expressions to strip from origin server responses before comparison")
+                      help="File containing XPath expressions to strip from "
+                           "origin server responses before comparison")
     parser.add_option("--target-noise-xpath-file",
-                      help="File containing XPath expressions to strip from target server responses before comparison")
+                      help="File containing XPath expressions to strip from "
+                           "target server responses before comparison")
 
-    parser.add_option("--profile", action="store_true", default=False, help="Use cProfile to run webcompare")
+    parser.add_option("--profile", action="store_true", default=False,
+                      help="Use cProfile to run webcompare")
 
     ignoreres = []              # why isn't this set by the parser.add_option above?
     (options, args) = parser.parse_args()
@@ -510,5 +508,6 @@ if __name__ == "__main__":
         profiler.print_stats(sort="cumulative")
 
         print
-        print "Dumped full cProfile data to webcompare.cprofile: try loading it with `python -mpstats webcompare.cprofile`"
+        print "Dumped full cProfile data to webcompare.cprofile: try loading",
+        print " it with `python -mpstats webcompare.cprofile`"
         print
